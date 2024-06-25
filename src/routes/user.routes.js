ebdac4435d4410334c4d81b6cb4bc4e3e74aaaf2
Pratham Goel <pratham.goel.22.183@gmail.com>
2024-06-26 00:53:53 +0530

    tokens

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 const router=Router()
 router.route("/register").post(
@@ -14,4 +14,8 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,logoutUser)          //verifyJWT is middleware
 export default router
